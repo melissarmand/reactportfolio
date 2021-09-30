b4a82e830c25b59add9bbc790e7c0a98b3f51512
melissarmand <82781751+melissarmand@users.noreply.github.com>
2021-09-30 16:10:21 -0400

    updated homepage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
-import {HashRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDom.render(
