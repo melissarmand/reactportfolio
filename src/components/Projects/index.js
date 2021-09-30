e62ff2718b1598af2ad0c77e8a1b1ce4df7955a6
melissarmand <82781751+melissarmand@users.noreply.github.com>
2021-09-30 15:47:23 -0400

    finished links to projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -11,7 +11,8 @@ function Projects() {
     return (
         <div className= 'container'>
             {data.map((d)=> (
-                    <div className="jumbotron text-white jumbotron-image shadow" style= {{backgroundImage: `url("https://images.unsplash.com/photo-1552152974-19b9caf99137?fit=crop&w=1350&q=80")`}} >
+                    <div className="jumbotron text-white jumbotron-image shadow" > 
+                    {/* //style= {{backgroundImage: `url("https://images.unsplash.com/photo-1552152974-19b9caf99137?fit=crop&w=1350&q=80")`}}  */}
                     <h2 className="mb-4 text-center" style= {{color: "green"}}>
                       {d.title}
                     </h2>
